2d25aec1234fdf7699406fda5b7884656cf1ad7f
Giselli Hara <gi@hara.ninja>
2021-07-09 17:41:41 -0300

    routes

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function ProductPrice() {
+    return (
+        <div className="product-price-container">
+            <span>R$</span>
+            <h3>1234,56</h3>
+        </div>
+    );
+}
+
+export default ProductPrice;
---
+++ frontweb/src/pages/Admin/index.tsx
@@ -0,0 +1,7 @@
+function Admin() {
+    return (
+        <h1>Página Admin</h1>
+    );
+}
+
+export default Admin;
